9f469f9d12f3ee5d99d518b33f25706625a4fa5d
exc24 <portnov.mrportnov@yandex.ru>
2022-08-11 17:20:56 +0300

    Initial commit

---
+++ app/src/main/java/com/example/tabbar/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.tabbar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tabbar.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container_view,CitiesListFragment.newInstance()).commit()
+    }
+}
